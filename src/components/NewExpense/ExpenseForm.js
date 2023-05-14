0b281941459b4d7911046217b88c5b831317c9b9
Danarzlf <Danarzlf10@example.com>
2023-05-14 15:31:31 +0700

    perubahan pada styling

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './ExpenseForm.css';
+import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate] = useState('');
-  const [enteredDescription, setEnteredDescription] = useState('');
-  const [enteredStock, setEnteredStock] = useState('');
-  const [enteredImageURL, setEnteredImageURL] = useState('');
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const [enteredDescription, setEnteredDescription] = useState("");
+  const [enteredStock, setEnteredStock] = useState("");
+  const [enteredImageURL, setEnteredImageURL] = useState("");
   // const [userInput, setUserInput] = useState({
   //   enteredTitle: '',
   //   enteredAmount: '',
@@ -73,85 +73,72 @@ const ExpenseForm = (props) => {
       title: enteredTitle,
       amount: enteredAmount,
       date: new Date(enteredDate),
-      description : enteredDescription,
-      stock : enteredStock,
-      imageURL : enteredImageURL
+      description: enteredDescription,
+      stock: enteredStock,
+      imageURL: enteredImageURL,
     };
 
     props.onSaveExpenseData(expenseData);
-    setEnteredTitle('');
-    setEnteredAmount('');
-    setEnteredDate('');
-    setEnteredDescription('');
-    setEnteredStock('');
-    setEnteredImageURL('');
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+    setEnteredDescription("");
+    setEnteredStock("");
+    setEnteredImageURL("");
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
-          {/* <h2>Add New Product</h2> */}
+      <h1>ADD PRODUCT</h1>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
           <label>Name Product</label>
-          <input
-            type='text'
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
+          <div>
+            <img src={require("../image/product.png")} />
+            <input type="text" value={enteredTitle} onChange={titleChangeHandler} placeholder="Book" />
+          </div>
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Price</label>
-          <input
-            type='number'
-            min='0.5'
-            step='0.5'
-            value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
+          <div>
+            <img src={require("../image/price.png")} />
+            <input type="number" min="0.5" step="0.5" value={enteredAmount} onChange={amountChangeHandler} placeholder="$70.5" />
+          </div>
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Date</label>
-          <input
-            type='date'
-            min='2019-01-01'
-            max='2022-12-31'
-            value={enteredDate}
-            onChange={dateChangeHandler}
-          />
+          <div>
+            <img src={require("../image/date.png")} />
+            <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
+          </div>
         </div>
-        <div className='new-expense__control'>
-          {/* <h2>Add New Product</h2> */}
+        <div className="new-expense__control">
           <label>Description</label>
-          <input
-            type='text'
-            value={enteredDescription}
-            onChange={descriptionChangeHandler}
-          />
+          <div>
+            <img src={require("../image/description.png")} />
+            <input type="text" value={enteredDescription} onChange={descriptionChangeHandler} placeholder="This book provides practical knowledge" />
+          </div>
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Stock</label>
-          <input
-            type='number'
-            min='1'
-            step='1'
-            value={enteredStock}
-            onChange={stockChangeHandler}
-          />
+          <div>
+            <img src={require("../image/stock.png")} />
+            <input type="number" min="1" step="1" value={enteredStock} onChange={stockChangeHandler} placeholder="100" />
+          </div>
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Image</label>
-          <input
-            type='file'
-            accept='.jpg,.png,.jpeg'
-            value={enteredImageURL}
-            onChange={imageURLChangeHandler}
-         />
+          <div>
+            <img src={require("../image/image.png")} />
+            <input type="file" accept=".jpg,.png,.jpeg" value={enteredImageURL} onChange={imageURLChangeHandler} />
+          </div>
         </div>
-
       </div>
-      <div className='new-expense__actions'>
-        <button type="button" onClick={props.onCancel}>Cancel</button>
-        <button type='submit'>Add Product</button>
+      <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit">Save</button>
       </div>
     </form>
   );
